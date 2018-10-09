c3aa298cdb6924adf4bc9e65f4f3464b229e3d79
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-09 16:29:00 -0700

    Add store

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.getState = store.getState; window.dispatch = store.dispatch;
+  // window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
+
   ReactDOM.render(<h1>Pokedex</h1>, root);
 });
