faeb4c49a12bfdf9c118ce86db806d92a327cf89
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-09 17:41:56 -0700

    Add thunk and selector

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
 import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
   window.getState = store.getState; window.dispatch = store.dispatch;
-  // window.requestAllPokemon = requestAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
   window.selectAllPokemon = selectAllPokemon;
 
   ReactDOM.render(<h1>Pokedex</h1>, root);
